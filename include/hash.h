a39f637bab437fe02f6ab4f9b467629b6572a8a6
Francesco <inbox@francescorocca.me>
2026-03-17 13:03:38 +0100

    style: clang-format

--- include/hash.h
+++ include/hash.h
@@ -1,11 +1,12 @@
-#ifndef UTILS_HASH_H
-#define UTILS_HASH_H
+#ifndef ASSEMBLER_HASH_H
+#define ASSEMBLER_HASH_H
 
 #include <stdint.h>
-#define OPCODES_NUM 64 //Way less but we keep some space for the future.
+
+#define OPCODES_NUM 64 // Way less but we keep some space for the future.
 
 unsigned long sdbm(char *keyword);
 
-//uint8_t opcodes[OPCODES_NUM];
+// uint8_t opcodes[OPCODES_NUM];
 
-#endif // UTILS_HASH_H
+#endif
