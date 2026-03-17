91fe8e3f00065abb0edf50c90412fcb1edac8540
Emiliano <em1l14n0s@gmail.com>
2026-03-17 12:25:19 +0100

    pre-first commit!

---
+++ src/hash.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_HASH_H
+#define UTILS_HASH_H
+
+#include <stdint.h>
+#define OPCODES_NUM 64 //Way less but we keep some space for the future.
+
+unsigned long sdbm(char *keyword);
+
+//uint8_t opcodes[OPCODES_NUM];
+
+#endif // UTILS_HASH_H
