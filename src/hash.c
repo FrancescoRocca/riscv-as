0c4b514ae02d5859438534debf13687c749b0560
Francesco <inbox@francescorocca.me>
2026-03-17 12:57:31 +0100

    refactor: adjust file structure

--- src/hash.c
+++ src/hash.c
@@ -3,13 +3,13 @@
 /* Simple hash sdbm function implementation */
 /* Implementation from gawk, not our copyright.*/
 
-unsigned long sdbm(char *keyword){
-    unsigned long hash = 0;
-    int c;
+unsigned long sdbm(char *keyword) {
+	unsigned long hash = 0;
+	int c;
 
-    while( c = *keyword++ ){
-        hash = c + ( hash << 6 ) + ( hash << 16 ) - hash;
-    }
+	while ((c = *keyword++)) {
+		hash = c + (hash << 6) + (hash << 16) - hash;
+	}
 
-    return hash;
+	return hash;
 }
--- src/main.c
+++ src/main.c
@@ -1,40 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "hash.h"
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define BUF_SIZE 256
 #define KEYWORD_SIZE 6
 
-int main(){
-    char line[BUF_SIZE];
-    //Open file
-    char opcode[KEYWORD_SIZE];
-    char reg1[KEYWORD_SIZE];
-    char reg2[KEYWORD_SIZE];
-    char reg3[KEYWORD_SIZE];
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		fprintf(stdout, "Usage: %s <FILE.s>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    uint8_t opcodes[OPCODES_NUM * 4];
-    for( int i = 0; i < OPCODES_NUM; i++ ){
-        opcodes[i] = 0;
-    }
-    FILE* f = fopen("test_opcodes.s","r");
-    if( f == NULL ){
-        printf("Error: no file found\n");
-        return -1;
-    }
-    while( fgets(line, sizeof(line), f)){
-        //sscanf(line,"%s %[^,], %[^,], %[^,]\n",opcode, reg1, reg2, reg3);
-        sscanf(line,"%s\n",opcode);
-        //printf("Opcode: %s Hash: %d\n", opcode, (sdbm(opcode) % 64));
-        opcodes[sdbm(opcode) % (OPCODES_NUM * 2)]++;
-    }
-    for(int i = 0; i < OPCODES_NUM * 2; i++){
-        printf("Index: %d Occurencies: %d \n", i, opcodes[i]);
-    }
-    //Read each line
-    //Parse each line
-    //Write output file
-    fclose(f);
-    return 0;
+	char line[BUF_SIZE];
+	// Open file
+	char opcode[KEYWORD_SIZE];
+	// char reg1[KEYWORD_SIZE];
+	// char reg2[KEYWORD_SIZE];
+	// char reg3[KEYWORD_SIZE];
+
+	uint8_t opcodes[OPCODES_NUM * 4];
+	for (int i = 0; i < OPCODES_NUM; i++) {
+		opcodes[i] = 0;
+	}
+	FILE *f = fopen(argv[1], "r");
+	if (f == NULL) {
+		printf("Error: no file found\n");
+		return -1;
+	}
+	while (fgets(line, sizeof(line), f)) {
+		// sscanf(line,"%s %[^,], %[^,], %[^,]\n",opcode, reg1, reg2, reg3);
+		sscanf(line, "%s\n", opcode);
+		// printf("Opcode: %s Hash: %d\n", opcode, (sdbm(opcode) % 64));
+		opcodes[sdbm(opcode) % (OPCODES_NUM * 2)]++;
+	}
+	for (int i = 0; i < OPCODES_NUM * 2; i++) {
+		printf("Index: %d Occurencies: %d \n", i, opcodes[i]);
+	}
+	// Read each line
+	// Parse each line
+	// Write output file
+	fclose(f);
+
+	return EXIT_SUCCESS;
 }
